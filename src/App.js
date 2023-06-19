960e4af0ed7f7c24330ea0dc487f42e627cdc6d8
tabpritam <tab.pritam@gmail.com>
2023-06-19 11:39:07 +0530

    search functionaliti implemented

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
+    console.log("constructor called");
   }
 
   componentDidMount() {
@@ -26,9 +28,24 @@ class App extends Component {
   }
 
   render() {
+    console.log("Rendered");
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
