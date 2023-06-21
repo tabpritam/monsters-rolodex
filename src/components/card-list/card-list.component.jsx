5d43cb13375c0adac869e2b9c99e8b74ec6184f2
tabpritam <tab.pritam@gmail.com>
2023-06-21 12:54:40 +0530

    all class based component changed to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.style.css";
 
-class CardList extends Component {
-  render() {
-    const { monster } = this.props;
-    return (
-      <div className="card-list">
-        {monster.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monster }) => (
+  <div className="card-list">
+    {monster.map((monster) => {
+      return <Card monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
